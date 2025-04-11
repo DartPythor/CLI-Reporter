55584e004a302d7f5d5c4b1989f7586a0c98ba90
Maxim <neverovm28@gmail.com>
2025-04-12 01:04:57 +0400

    feat: Added validation for --report

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ from handlers_report import (
 
 
 class ConfigFabric:
+    config_report = {
+        "handlers": {
+            "validator": LogValidator("django.request"),
+            "parser": LogParser(
+                r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}\s+"
+                r"(\w+)\s+django\.request:\s+"
+                r"(?:.*?(?:GET|POST|PUT|DELETE|PATCH)\s+(\S+)|.*?:\s+(\S+))",
+            ),
+            "collector": CollectorData(),
+            "printer": ReportPrinter(),
+        },
+    }
     @staticmethod
     def get(report_name: str) -> dict:
-        config_report = {
-            "handlers": {
-                "validator": LogValidator("django.request"),
-                "parser": LogParser(
-                    r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}\s+"
-                    r"(\w+)\s+django\.request:\s+"
-                    r"(?:.*?(?:GET|POST|PUT|DELETE|PATCH)\s+(\S+)|.*?:\s+(\S+))",
-                ),
-                "collector": CollectorData(),
-                "printer": ReportPrinter(),
-            },
-        }
-        return config_report[report_name]
+        return ConfigFabric.config_report[report_name]
 
 
 def parse_args() -> Namespace:
@@ -46,10 +46,17 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-def validate_files(files) -> None:
+def validate_files(files: str) -> None:
     for file in files:
         if not pathlib.Path(file).exists():
             raise FileNotFoundError(f"File not found: {file}")
+
+
+def validate_report(report: str) -> None:
+    if report not in ConfigFabric.config_report:
+        raise Exception(
+            f"Unknown report given! Choose from: {ConfigFabric.config_report.keys()}",
+        )
 
 
 def process_single_file(file_path: str, config: str) -> dict:
@@ -65,7 +72,7 @@ def process_single_file(file_path: str, config: str) -> dict:
 def main():
     args = parse_args()
     validate_files(args.files)
-
+    validate_report(args.report)
     with multiprocessing.Pool() as pool:
         process_func = partial(process_single_file, config=args.report)
         results = pool.map(process_func, args.files)
